01f8668ed492c15e621f999c85a97cac56c65b8c
Estoyair <drhallevrda@gmail.com>
2020-09-15 18:51:11 +0000

    added some prints and got stuck on bools and types

--- main.swift
+++ main.swift
@@ -10,25 +10,37 @@ print ("it didnt compile because let cannot be changed")
 
 // Note: These exercises reinforce Swift concepts in the context of a fitness tracking app.
 // Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000.
-
+let goalSteps = 10000 
 
 // Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
 
+print ("Your step goal for the day is 5000")
+print (goalSteps)
 
 // Declare a variable schooling and set it to the number of years of school that you have completed. Print schooling to the console.
 
+var schooling = 12
+print (schooling)
 
 // Now imagine you just completed an additional year of school, and update the schooling variable accordingly. Print schooling to the console.
 
+schooling = 13
+print (schooling)
 
 // Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the print function.
-
+print ("it compiles because var can be overridden and a let cannot")
 
 // Create a variable called steps that will keep track of the number of steps you take throughout the day. Set its initial value to 0 to represent the step count first thing in the morning. Print steps to the console.
 
+var steps = 0
+
+print(steps)
 
 // Now assume the tracker has been keeping track of steps all morning, and you want to show the user the latest step count. Update steps to be 2000. Print steps to the console. Then print "Good job! You're well on your way to your daily goal."
 
+steps = 2000
+print(steps)
+print ("Good job! You're well on your way to your daily goal.")
 
 /* Imagine you're creating a simple photo sharing app. You want to keep track of the following metrics for each post:
 
@@ -41,6 +53,17 @@ Day created: The day of the month the post was created
 For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
 */
 
+//var likes
+print("likes change almost daily")
+//var comments
+print("new comments can be addded everyday")
+//let yearCreated
+print("the year something is made wont change")
+//let monthCreated
+print("the same aplies to the month something was created in")
+//let dayCreated
+print("finally the same idea aplies here too")
+
 
 /* There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
 
@@ -51,14 +74,28 @@ Goal number of steps: The user's goal for number of steps to take each day
 Average heart rate: The user's average heart rate over the last 24 hours
 */
 
+//let name
+print ("once they pick their name i think they should stick with it")
+//var age
+print("age changes every year so i made it a var")
+//var stepsTaken
+print("the amount of steps you take is always changing")
+//let stepsGoal
+print("goals should be consistent")
+//var heartRate
+print("much like age it changes often except heartrate changes daily")
+
 // Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
 
 
 // Type Safety
 // Declare two variables, one called firstDecimal and one called secondDecimal. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
 
+var firstDecimal = 2.2
+var secondDecimal = 3.2
 
 // Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+
 
 
 // Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
